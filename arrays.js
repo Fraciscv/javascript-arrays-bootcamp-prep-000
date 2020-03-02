7e87974ced8590e5bd58e70873d7744fac9d4130
Francisco Andaur <francisco.andaur@icloud.com>
2020-03-02 18:16:44 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,40 +1,12 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray(array, element){
-  return [element, ...array];//does not modify original
+functions addElementToBeginningOfArray(chocolateBars, element){
+  return [chocolateBars, ...element];
+
 }
 
-function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element);//element is now begingin, alters original array
-  return array;
-}
+functions destructivelyAddElementToBeginningOfArray(chocolateBars, element){
+  chocolateBars.unshift(element);
+  return chocolateBars;
 
-function addElementToEndOfArray(array, element){
-  return [...array, element];
-}
-
-function destructivelyAddElementToEndOfArray(array, element){
-  array.push(element);
-  return array;
-}
-
-function accessElementInArray(array, i){
-  return array[i];
-}
-
-function destructivelyRemoveElementFromBeginningOfArray(array){
-  array.shift();
-  return array;
-}
-
-function removeElementFromBeginningOfArray(array){
-  return array.slice(1);
-}
-
-function destructivelyRemoveElementFromEndOfArray(array){
-  array.pop();
-  return array;
-}
-function removeElementFromEndOfArray(array){
-  return array.slice(0, array.length-1);
 }
