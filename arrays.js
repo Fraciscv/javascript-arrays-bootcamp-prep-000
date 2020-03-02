9589845c3a43ecc5db656af5cc502edfdef08917
Francisco Andaur <francisco.andaur@icloud.com>
2020-03-02 18:20:38 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,12 +1,2 @@
-const chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-functions addElementToBeginningOfArray(array, element) {
-  return [array, ...element];
-
-}
-
-functions destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element);
-  return array;
-
-}
